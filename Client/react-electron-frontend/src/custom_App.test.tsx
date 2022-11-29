b8d342fdecd11f1748d77df5b32ecc09fd0e86be
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-11-29 20:36:37 +0100

    Revert "small cahnges on react gui"
    This reverts commit 5de7bdf3f0b717740538654e670cad95e8c87755.

--- Client/react-electron-frontend/src/custom_App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Custom_App from './custom_App';
-
-test('renders learn react link', () => {
-  render(<Custom_App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
